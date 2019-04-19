299cfacb4dbe96026421ada4e4fd0c5b60805ae4
Jose <je.dominguez.vidal@gmail.com>
2019-04-19 12:19:27 +0200

    Options Bar added (graphic interface only)

--- layout.py
+++ layout.py
@@ -1,8 +1,37 @@
 from tkinter import *
 from tkinter import messagebox
-import operations
+import operations as op
+
+'''
+Main class. It implements the main layout of the application.
+Each functionality use one or several functions from "operations.py" module.
+'''
+
+# ----- Graphic Interface -----
+root = Tk()
+
+# ----- Options Bar -----
+barMenu = Menu(root)
+root.config(menu=barMenu, width=300, height=300)
+# File menu
+fileMenu = Menu(barMenu, tearoff = 0)
+fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Save As...")
+fileMenu.add_command(label="Clear")
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit")
+# App menu
+appMenu = Menu(barMenu, tearoff = 0)
+appMenu.add_command(label="Hide text")
+appMenu.add_command(label="Recover text")
+# Help menu
+helpMenu = Menu(barMenu, tearoff = 0)
+helpMenu.add_command(label="Help")
+helpMenu.add_command(label="About")
+
+barMenu.add_cascade(label="File", menu=fileMenu)
+barMenu.add_cascade(label="App", menu=appMenu)
+barMenu.add_cascade(label="Help", menu=helpMenu)
 
 
-print("Hello world")
-a = operations.testFunction(2, 3)
-print(a)
+root.mainloop()
