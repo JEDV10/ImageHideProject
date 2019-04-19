ec7a59a30e73eb0b8ba897c0c32ac54ae1ab8827
Jose <je.dominguez.vidal@gmail.com>
2019-04-19 11:47:25 +0200

    First commit

---
+++ layout.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import messagebox
+import operations
+
+
+print("Hello world")
+a = operations.testFunction(2, 3)
+print(a)
---
+++ operations.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def testFunction (num1, num2):
+    return np.ones((num1, num2)).astype(int)
