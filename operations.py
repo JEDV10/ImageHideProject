cce3ae7e1b944320569fc24cf377bb96501594be
J. Enrique <je.dominguez.vidal@gmail.com>
2019-04-30 11:03:12 +0200

    Code cleanning

--- operations.py
+++ operations.py
@@ -1,11 +1,10 @@
 import math
 from tkinter import *
 from tkinter import filedialog
-from tkinter import messagebox
 from datetime import datetime
 from PIL import Image
-import numpy as np
 from skimage import io
+import numpy as np
 
 
 # ----- Variables -----
@@ -28,7 +27,7 @@ def deleteFields(inputImagePath, charCounter, textBlock):
     Erase all data in each input field
     """
     inputImagePath.set("")
-    charCounter.set("")
+    charCounter.set("0/0 (0)")
     textBlock.delete(1.0, END)
 
 
@@ -57,7 +56,7 @@ def saveText(text):
     Creates a ".txt" file
     """
     date_string = obtainDateString()
-    filename = filedialog.asksaveasfilename(initialfile= "ImageHide_text_"+date_string+".txt",
+    filename = filedialog.asksaveasfilename(initialfile="ImageHide_text_" + date_string + ".txt",
                                             defaultextension=".txt", title="Select file", initialdir="C:",
                                             filetypes=(("Text File", "*.txt"), ("All Files", "*.*")))
     try:
@@ -81,20 +80,6 @@ def saveImage(initial_image_path, image):
                                             filetypes=(("Images", "*.png"), ("All Files", "*.*")))
     image_uint8 = np.array(image, dtype=np.uint8) # Take care of this line. uint8 can convert 254 in 255.
     io.imsave(filename, image_uint8)
-    messagebox.showinfo("Info", "Modified image saved correctly.")
-
-
-def saveImage_old(initial_image_path, image):
-    """
-    Save new image with hidden message.
-    Creates a ".png" file
-    """
-    date_string = obtainDateString()
-    filename = filedialog.asksaveasfilename(initialfile="ImageHide_image_" + date_string + ".png",
-                                            defaultextension=".png", title="Select file",
-                                            initialdir=initial_image_path,
-                                            filetypes=(("Images", "*.png"), ("All Files", "*.*")))
-    image.save(filename)
     messagebox.showinfo("Info", "Modified image saved correctly.")
 
 
@@ -161,6 +146,9 @@ def updateCharCounter(textBlock, charCounter, image_path, charLabel):
 
 
 
+
+
+
 # ----- Hide Text Operations -----
 def obtainAsciiRepresentation(char):
     """
@@ -168,31 +156,13 @@ def obtainAsciiRepresentation(char):
     """
     return ord(char)
 
+
 def obtainBinaryRepresentation(num):
     """
     Return binary representation of given number (16 bits format)
     """
     return bin(num)[2:].zfill(16)
 
-def changeLSB(byte, new_bit):
-    """
-    Change LSB of given byte
-    """
-    return byte[:-1] + str(new_bit)
-
-def binaryToDecimal(binary):
-    """
-    Return decimal representation of given binary number
-    """
-    return int(binary, 2)
-
-def changeColor(original_color, bit):
-    """
-    Change LSB of given byte
-    """
-    binary_color = obtainBinaryRepresentation(original_color)
-    mod_color = changeLSB(binary_color, bit)
-    return binaryToDecimal(mod_color)
 
 def obtainBitsList(text):
     """
@@ -206,11 +176,13 @@ def obtainBitsList(text):
             list.append(bit)
     return list
 
+
 def obtainInitOutputImagePath(original_path):
     """
     Return path to folder where is the original path
     """
     return original_path[:(original_path.rfind("/"))]
+
 
 def hideText(message, original_image_path):
     """
@@ -252,77 +224,17 @@ def hideText(message, original_image_path):
         pass
 
 
-def hideText_old(message, original_image_path):
-    """
-    Main function to hide text inside images.
-    It opens the image at given path, converts the given message to binary and
-    modify LSBs of each pixel in the image acording to the binary representation
-    of the original message
-    """
-    try:
-        image = Image.open(original_image_path)
-        pixels = image.load()
 
-        (columns, rows) = image.size
 
-        list = obtainBitsList(head_string + message + end_string)
-        counter = 0
-        total_lenght = len(list)
-        for x in range(rows):
-            for y in range(columns):
-                if counter < total_lenght:
-                    pixel = pixels[x, y]
-                    red = pixel[0]
-                    green = pixel[1]
-                    blue = pixel[2]
-
-                    if counter < total_lenght:
-                        mod_red = changeColor(red, list[counter])
-                        counter += 1
-                    else:
-                        mod_red = red
-
-                    if counter < total_lenght:
-                        mod_green = changeColor(green, list[counter])
-                        counter += 1
-                    else:
-                        mod_green = green
-
-                    if counter < total_lenght:
-                        mod_blue = changeColor(blue, list[counter])
-                        counter += 1
-                    else:
-                        mod_blue = blue
-
-                    pixels[x, y] = (mod_red, mod_green, mod_blue)
-                else:
-                    break
-            else:
-                continue
-            break
-
-        if (counter < total_lenght):
-            messagebox.showwarning("Warning", "Warning: {} remaining chars."
-                  .format(math.floor((total_lenght - counter) / 16)))
-
-        initial_image_path = obtainInitOutputImagePath(original_image_path)
-        saveImage_old(initial_image_path, image)
-    except OSError:
-        messagebox.showwarning("Warning", "Select a valid image file.")
 
 
 # ----- Recover Text Operations -----
-def obtainLSB(byte):
-    """
-    Obtain LSB of given byte
-    """
-    return byte[-1]
-
 def charFromAscii(numero):
     """
     Return char representation of given decimal number
     """
     return chr(numero)
+
 
 def obtainModifiedImagePath():
     """
@@ -330,6 +242,7 @@ def obtainModifiedImagePath():
     """
     return filedialog.askopenfilename(title="Open", initialdir="C:",
                                          filetypes=(("Images", "*.png"), ("All Files", "*.*")))
+
 
 def recoverText(textBlock):
     """
@@ -374,73 +287,3 @@ def recoverText(textBlock):
         messagebox.showwarning("Warning", "Select a valid image file.")
     except ValueError:
         pass
-
-
-def recoverText_old(textBlock):
-    """
-    Main function to recover text from images.
-    It opens the modified image at given path, extracts LSBs from each pixel and
-    reconstruct the message until it finds termination string
-    """
-    try:
-        hidden_text_image_path = obtainModifiedImagePath()
-        image = Image.open(hidden_text_image_path)
-        pixels = image.load()
-
-        (columns, rows) = image.size
-
-        byte = ""
-        message = ""
-        found = False
-        for x in range(rows):
-            for y in range(columns):
-                pixel = pixels[x, y]
-                red = pixel[0]
-                green = pixel[1]
-                blue = pixel[2]
-
-                byte += obtainLSB(obtainBinaryRepresentation(red))
-                if len(byte) >= 16:
-                    message += charFromAscii(binaryToDecimal(byte))
-                    if (message[-len(head_string):] == head_string):
-                        message = ""
-                        found = True
-                    if (message[-len(end_string):] == end_string):
-                        message = message[0:len(message) - len(end_string)]
-                        break
-                    byte = ""
-
-                byte += obtainLSB(obtainBinaryRepresentation(green))
-                if len(byte) >= 16:
-                    message += charFromAscii(binaryToDecimal(byte))
-                    if (message[-len(head_string):] == head_string):
-                        message = ""
-                        found = True
-                    if (message[-len(end_string):] == end_string):
-                        message = message[0:len(message) - len(end_string)]
-                        break
-                    byte = ""
-
-                byte += obtainLSB(obtainBinaryRepresentation(blue))
-                if len(byte) >= 16:
-                    message += charFromAscii(binaryToDecimal(byte))
-                    if (message[-len(head_string):] == head_string):
-                        message = ""
-                        found = True
-                    if (message[-len(end_string):] == end_string):
-                        message = message[0:len(message) - len(end_string)]
-                        break
-                    byte = ""
-
-            else:
-                continue
-            break
-
-        if found:
-            textBlock.delete(1.0, END)
-            textBlock.insert('1.0', message)
-            messagebox.showinfo("Info", "Hidden text recovered correctly.")
-        else:
-            messagebox.showwarning("Warning", "No hidden text found. Try other image.")
-    except AttributeError:
-        messagebox.showwarning("Warning", "Select a valid image file.")
